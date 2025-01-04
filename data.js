f2cb2d7a0c345d64914b1bc03b2d9dbe7c131792
YashMandlik722 <mandlikyash722@gmail.com>
2025-01-04 14:26:01 +0530

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        "Sno": 1,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 2,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 3,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 4,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 5,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 1,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 1,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 1,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 1,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    },
+    {
+        "Sno": 1,
+        "title": "t1",
+        "date":"01/01/2025",
+        "priority": "1"
+    }
+]
